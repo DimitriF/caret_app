8848b60759f51fe3a0c36f7b26dc95aca5ed1608
DimitriF <dimitrifichou@laposte.net>
2016-05-05 11:52:27 +0200

    first commit

---
+++ server.R
@@ -0,0 +1,115 @@
+# Application to facilitate machine learning training with the caret package
+
+
+library(shiny)
+library(caret)
+library(plyr)
+library(magrittr)
+library(readxl)
+
+
+shinyServer(function(input, output) {
+
+  output$caret_table <- renderDataTable({
+    caret.table <- cbind(
+      llply(getModelInfo(),function(l){l$label}),
+      llply(getModelInfo(),function(l){l$type})
+    )
+    type <- rep(0,nrow(caret.table))
+    for(i in seq(nrow(caret.table))){
+      if(caret.table[,2][i] == "Classification"){type[i] <- "Classification"}
+      if(caret.table[,2][i] == "Regression"){type[i] <- "Regression"}
+      if(length(unlist(caret.table[,2][i])) == 2){type[i] <- "Both"}
+    }
+    data.frame(name = unlist(caret.table[,1]),type)
+  })
+  
+  output$algo.info <- renderPrint({
+    getModelInfo()[input$algo]
+  })
+  
+  Dep <- reactive({
+    if(is.null(input$Dep)){
+      iris$Species
+    }else{
+      read_excel(input$Dep$datapath)
+    }
+  })
+  output$Dep <- renderTable({
+    Dep() %>% as.data.frame
+  })
+  Ind <- reactive({
+    if(is.null(input$Ind)){
+      iris[,1:4]
+    }else{
+      read_excel(input$Dep$datapath)
+    }
+  })
+  output$Ind <- renderTable({
+    Ind()
+  })
+  output$grid <- renderTable({
+    grid <- getModelInfo(model = input$algo)[[input$algo]]$grid
+    grid(Ind(),Dep(),len=input$tuning.length)
+  })
+  
+  output$Train.metric <- renderUI({
+    if(input$Trainproblem == 'Classification' & length(unique(Dep())) == 2){
+      truc <- c('Accuracy','Kappa','Specificity','Sensitivity','Pos_Pred_Value','Neg_Pred_Value','Detection_Rate','Balanced_Accuracy')
+    }
+    if(input$Trainproblem == 'Classification'& length(unique(Dep())) > 2){
+      truc <- c('Accuracy','Kappa','Mean_Sensitivity','Mean_Specificity','Mean_Pos_Pred_Value','Mean_Neg_Pred_Value','Mean_Detection_Rate','Mean_Balanced_Accuracy')
+      names(truc) <- c('Accuracy','Kappa','Specificity','Sensitivity','Pos_Pred_Value','Neg_Pred_Value','Detection_Rate','Balanced_Accuracy')
+    }
+    if(input$Trainproblem == 'Regression'){
+      truc <- c('RMSE','Rsquared')
+    }
+    selectizeInput('Train.metric','what summary metric will be used to select the optimal model',choices=truc)
+  })
+  
+  model <- eventReactive(input$Go,{
+    # validate(need(nrow(Ind) == nrow(as.data.frame(Dep()))),"The number of observation in the two files are differents")
+    withProgress(message = "Work in Progress", value=0, {
+      incProgress(0)
+      data <- data.frame(Ind = Ind(), Dep = Dep())
+      set.seed(1)
+      if(input$Trainproblem == 'Classification'){
+        control <- trainControl(method = input$Train.control.method,
+                                number=input$Train.tunning.CV,
+                                savePredictions = "final",
+                                summaryFunction = multiClassSummary,
+                                allowParallel=T,verboseIter=F,returnData=F)
+      }
+      if(input$Trainproblem == 'Regression'){
+        control <- trainControl(method = input$Train.control.method,
+                                number=input$Train.tunning.CV,
+                                savePredictions = "final",
+                                summaryFunction = defaultSummary,
+                                allowParallel=T,verboseIter=F,returnData=F)
+      }
+      set.seed(1)
+      model <- train(Dep ~. , data = data,
+                     method=input$algo,
+                     metric = input$Train.metric,
+                     trControl = control,
+                     tuneLength = input$tuning.length
+      )
+    })
+    return(model)
+  })
+  
+  output$model.print <- renderPrint({
+    print(model())
+    str(model())
+  })
+  
+  output$model.plot <- renderPlot({
+    plot(model())
+  })
+  
+  output$pred.table <- renderTable({
+    x <- model()$pred$obs
+    y <- model()$pred$pred
+    data.frame(obs=x,pred=y)
+  })
+})
---
+++ ui.R
@@ -0,0 +1,70 @@
+# Application to facilitate machine learning training with the caret package
+# Avoid the step learning curve of all ML framework
+# 2 xlsx files to upload, need default to iris in case no upload
+
+
+library(shiny)
+library(caret)
+library(plyr)
+
+caret.table <- cbind(
+  llply(getModelInfo(),function(l){l$label}),
+  llply(getModelInfo(),function(l){l$type})
+)
+Train.model.algo.choice <- names(caret.table[,1])
+names(Train.model.algo.choice) <- caret.table[,1]
+
+shinyUI(fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      fileInput("Ind","Independent variable file",accept = "xlsx"),
+      fileInput("Dep","Dependent variable file",accept = "xlsx"),
+      selectizeInput("algo",'Choice of the algorythm',choices= Train.model.algo.choice,selected='rf'),
+      radioButtons('Trainproblem','Type',choices=c('Classification','Regression'),selected='Classification'),
+      numericInput("tuning.length","Tuning length",10),
+      actionButton('Go','Train'),
+      helpText(   a("Click Here to learn about the Validation techniques",target="_blank",
+                    href="https://en.wikipedia.org/wiki/Cross-validation_%28statistics%29")
+      ),
+      selectizeInput('Train.control.method','Validation method for the tunning',
+                     choices=c('boot', 'repeatedcv', 'LOOCV'),
+                     selected='repeatedcv'),
+      uiOutput('Train.metric'),
+      numericInput('Train.tunning.CV','Either the number of folds or number of resampling iterations',5)
+    ),
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Prediction table",
+                 h4("This table is the cross validation table"),
+                 tableOutput("pred.table")
+                 ),
+        tabPanel("Model print",
+                 verbatimTextOutput("model.print")
+        ),
+        tabPanel("Training plot",
+                 plotOutput("model.plot")
+        ),
+        tabPanel("Tuning grid",
+                 tableOutput("grid")
+                 ),
+        tabPanel("Data",
+                 column(8,
+                        h4("Independent variables"),
+                        tableOutput("Ind")
+                        ),
+                 column(4,
+                        h4("Dependent variables"),
+                        tableOutput("Dep")
+                 )
+                 ),
+        tabPanel("Model list",
+                 dataTableOutput("caret_table")
+                 ),
+        tabPanel("Model Info",
+                 verbatimTextOutput("algo.info")
+                 )
+      )
+      
+    )
+  )
+))
